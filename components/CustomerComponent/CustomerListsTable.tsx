411b5e882d225851b837a08d0ed8c2e35af38441
thantzinwin <thant.zin.winnnn@gmail.com>
2023-06-01 00:08:49 +0630

    fix:table in responsiv

--- components/CustomerComponent/CustomerListsTable.tsx
+++ components/CustomerComponent/CustomerListsTable.tsx
@@ -1,7 +1,14 @@
 import React, { useMemo, useState } from "react";
-import { Checkbox, Divider, MediaQuery, Table,Pagination } from "@mantine/core";
+import {
+  Checkbox,
+  Divider,
+  MediaQuery,
+  Table,
+  Pagination,
+  ScrollArea,
+} from "@mantine/core";
 import { customerLists } from "@/data/Data";
-import { useDisclosure } from '@mantine/hooks';
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import * as Avatar from "@radix-ui/react-avatar";
 // import EditDialog from "../utils/EditDialog";
 import ModalDialog from "../utils/ModalDialog";
@@ -10,29 +17,32 @@ import CustomerEditModal from "./CustomerEditModal";
 
 const CustomerListsTable = () => {
   const [opened, { open, close }] = useDisclosure(false);
-  const [editingUser,setEditingUser] = useState<any>(null)
+  const [editingUser, setEditingUser] = useState<any>(null);
+  const xsScreen = useMediaQuery("(min-width:36em)");
+  const smScreen = useMediaQuery("(min-width:40em)");
+  const mdScreen = useMediaQuery("(min-width:48em)");
+  const lgScreen = useMediaQuery("(min-width:64em)");
+  const xlScreen = useMediaQuery("(min-width:75em)");
+  const xl2Screen = useMediaQuery("(min-width:88em)");
 
-  const editUser = (user:any)=>{
-    console.log("user",user)
-    setEditingUser(user)
-    open()
-  
-  }
-
-
+  const editUser = (user: any) => {
+    console.log("user", user);
+    setEditingUser(user);
+    open();
+  };
 
   const [opened2, { open: open2, close: close2 }] = useDisclosure(false);
 
   const rows = customerLists.map((customer) => {
     return (
       <tr key={customer.id}>
-        <td scope="col" className="pl-16">
-          <Checkbox
-            // checked={selection.includes(row.id)}
-            // onChange={() => toggleRow(row.id)}
-            transitionDuration={0}
-          />
-        </td>
+        {/*<td scope="col" className="pl-16">*/}
+        {/*  <Checkbox*/}
+        {/*    // checked={selection.includes(row.id)}*/}
+        {/*    // onChange={() => toggleRow(row.id)}*/}
+        {/*    transitionDuration={0}*/}
+        {/*  />*/}
+        {/*</td>*/}
         <td>
           {" "}
           <div className="flex gap-1 items-center">
@@ -65,10 +75,14 @@ const CustomerListsTable = () => {
           </div>
         </td>
         <td>
-          <button onClick={()=>{
-            editUser(customer)
-            
-          }} className="text-blue-600 font-semibold">Edit User</button>
+          <button
+            onClick={() => {
+              editUser(customer);
+            }}
+            className="text-blue-600 font-semibold"
+          >
+            Edit User
+          </button>
           {/* <EditDialog user={customer}/> */}
           {/* <ModalDialog/> */}
         </td>
@@ -77,50 +91,56 @@ const CustomerListsTable = () => {
   });
 
   return (
-    <>
-      <section className="mt-4">
-      <Table className="shadow-lg bg-white rounded-lg"  verticalSpacing={"sm"} horizontalSpacing={"lg"} highlightOnHover fontSize={"xs"}>
-        <thead>
-          <tr className="uppercase bg-slate-200">
-            <th scope="col" className="pl-16">
-              <Checkbox
-                onChange={() => {}}
-                // indeterminate={
-                //   selection.length > 0 && selection.length !== tableData.length
-                // }
-                // checked={selection.length === tableData.length}
-                transitionDuration={0}
-              />
-            </th>
-            <th>CustomerName</th>
-            <th>NRC</th>
-            <th>Phone NO</th>
-            <th>Email Address</th>
-            <th>Address</th>
-            <th>Created Date</th>
-            <th>Gender</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
-      <Divider size="xs" />
-			<MediaQuery largerThan="sm" styles={{ display: "none" }}>
-				<div className=" w-[250px] ml-auto py-6">
-					<Pagination total={5} size={"sm"} />
-				</div>
-			</MediaQuery>
-			<MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
-				<div className=" w-[400px] ml-auto py-6">
-					<Pagination total={5} size={"lg"} />
-				</div>
-			</MediaQuery>
-    </section>
-    {editingUser && (
+    <div className="mt-4 shadow-xl bg-white rounded-lg w-full h-full">
+        <Table
+          className="rounded-lg overflow-hidden"
+          verticalSpacing={"sm"}
+          horizontalSpacing={"lg"}
+          highlightOnHover
+          fontSize={"xs"}
+        >
+          <thead>
+            <tr className="uppercase bg-slate-200">
+              {/*<th scope="col" className="pl-16">*/}
+              {/*  <Checkbox*/}
+              {/*    onChange={() => {}}*/}
+              {/*    // indeterminate={*/}
+              {/*    //   selection.length > 0 && selection.length !== tableData.length*/}
+              {/*    // }*/}
+              {/*    // checked={selection.length === tableData.length}*/}
+              {/*    transitionDuration={0}*/}
+              {/*  />*/}
+              {/*</th>*/}
+              <th>CustomerName</th>
+              <th>NRC</th>
+              <th>Phone NO</th>
+              <th>Email Address</th>
+              <th>Address</th>
+              <th>Created Date</th>
+              <th>Gender</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+
+        <Divider size="xs" />
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <div className=" w-[250px] ml-auto py-6">
+            <Pagination total={5} size={"sm"} />
+          </div>
+        </MediaQuery>
+        <MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
+          <div className=" w-[400px] ml-auto py-6">
+            <Pagination total={5} size={"lg"} />
+          </div>
+        </MediaQuery>
+     
+      {editingUser ? (
       <CustomerEditModal opened={opened} onClose={close} user={editingUser}/>
-    )}
-    </>
+    ): null}
+    </div>
   );
 };
 
--- pages/customer/index.tsx
+++ pages/customer/index.tsx
@@ -1,18 +1,67 @@
 import Button from "@/components/CustomerComponent/Button";
 import Header from "@/components/Header/Header";
-import React from "react";
+import React, { useState } from "react";
 import { IconPlus, IconDownload } from "@tabler/icons-react";
 import CustomerListsTable from "@/components/CustomerComponent/CustomerListsTable";
 import DownloadDropdown from "@/components/CustomerComponent/DownloadDropdown";
 import Layout from "../../components/utils/layout";
 import { useRouter } from "next/router";
+import {  Checkbox, ScrollArea, Table } from "@mantine/core";
+import { customerLists } from "@/data/Data";
+import * as Avatar from "@radix-ui/react-avatar";
+import Example from "@/components/CustomerComponent/MantineCustomerTable";
 
 const CustomerLists = () => {
   const router = useRouter()
+  const [scrolled, setScrolled] = useState(false);
+  const [editingUser, setEditingUser] = useState<any>(null);
+  const editUser = (user: any) => {
+    console.log("user", user);
+    setEditingUser(user);
+    open();
+  };
+
+
+
+
   return (
-    <Layout>
-      <section className="pl-8 pr-8 pt-8">
-        <div className="flex justify-between items-center w-full">
+    
+      
+       <Layout>
+       {/* <section className="pl-4 pr-3 overflow-hidden">
+       <div className="flex justify-between items-center w-full" >
+          <h2 className="text-2xl font-semibold mt-3 text-gray-800">
+            Customer Lists
+          </h2>
+          <div className="flex items-center gap-3">
+            <Button
+              bg="bg-blue-700"
+              textColor="text-white"
+              text="Create Customer"
+              padding="px-5 py-2 rounded-md"
+              icon={<IconPlus size={16} />}
+              handler={()=> router.push('/customer/create')}
+            />
+            <DownloadDropdown />
+          </div>
+          
+        </div>
+        <Example/>
+       </section> */}
+       <div className="overflow-x-scroll">
+       <Example/>
+       </div>
+        
+       </Layout>
+     
+    
+  );
+};
+
+export default CustomerLists;
+{/* <Layout>
+       <div className="pt-8 pr-8 pl-6 pb-6 h-full">
+       <div className="flex justify-between items-center w-full " >
           <h2 className="text-2xl font-semibold mt-3 text-gray-800">
             Customer Lists
           </h2>
@@ -29,9 +78,5 @@ const CustomerLists = () => {
           </div>
         </div>
         <CustomerListsTable />
-      </section>
-    </Layout>
-  );
-};
-
-export default CustomerLists;
+       </div>
+       </Layout> */}
